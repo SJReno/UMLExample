9e46735a070e676599f7e7ecc9f82b31d13d854d
monkey <monkey@BB21016>
2016-11-28 14:43:19 -0600

    Add project files.

---
+++ UMLExample/Profession.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UMLExample
+{
+    public abstract class Profession
+    {
+        public void GoToWork()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ UMLExample/SoftwareEngineer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UMLExample
+{
+    public class SoftwareEngineer : Profession, IProgrammer, IManager
+    {
+    }
+}
